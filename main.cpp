a6d055d035207e1ebdb521df116a6163ee2a4ea5
Franco <franco@wemake.pe>
2022-09-24 16:08:34 -0500

    Update main with new chapter1 code

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 #include "vec3.h"
 
 int main(){
-	int nx = 200;
-	int ny = 100;
+	const int image_width = 256;
+	const int image_height = 256;
 
-	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	for (int j = ny-1; j >= 0; j--){
-		for(int i = 0; i < nx; i++){
-			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.6);
-			int ir = int(255.99*col[0]);
-			int ig = int(255.99*col[1]);
-			int ib = int(255.99*col[2]);
+	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+	for (int j = image_height-1; j >= 0; j--){
+		std::cerr << "\rScanlines reamining: " << j << ' ' << std::flush;
+		for(int i = 0; i < image_width; i++){
+			vec3 col(double(i) / double(image_height), double(j) / double(image_width), 0.25);
+			int ir = static_cast<int>(255.999*col[0]);
+			int ig = static_cast<int>(255.999*col[1]);
+			int ib = static_cast<int>(255.999*col[2]);
+
 			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
 	}
+	std::cerr << "\nDone.\n";
 }
