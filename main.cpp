c6f1cf4762a71de91970824634f95d4e0f977a69
Franco <franco@wemake.pe>
2022-09-25 12:42:05 -0500

    Add world, hittable list, chapter 6

--- main.cpp
+++ main.cpp
@@ -1,37 +1,20 @@
+#include "rtweekend.h"
 #include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r){
-	vec3 oc = r.origin() - center;
-
-	auto a = r.direction().length_squared();
-	auto half_b = dot(oc, r.direction());
-	auto c = oc.length_squared() - radius * radius;
-	auto discriminant = half_b*half_b - a*c;
-	// getting discriminant to know if it touches the sphere or not
-	if (discriminant < 0){
-		return -1.0; // it doesnt hit
-	} else{
-		return (-half_b - sqrt(discriminant)) / a;
-	}
-}
-
 // get the color of the ray
-color ray_color(const ray &r){
-	auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-	if (t > 0.0){
-		// it hits the spehre get a color
-		vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-		return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray &r, const hittable& world){
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec)){
+		return 0.5 * (rec.normal + color(1,1,1));
 	}
-
-	// its the background
 	vec3 unit_direction = unit_vector(r.direction());
+
 	auto opacity = 0.5;
-	t = (unit_direction.y() + 1.0) * opacity;
+	auto t = (unit_direction.y() + 1.0) * opacity;
 	// blended value = (1-t) * startValue + t * endValue;
 	return (1.0-t)*color(1.0, 1.0,1.0) + t * color (0.5, 0.7, 1.0);
 }
@@ -42,6 +25,11 @@ int main(){
 	const int image_width = 400;
 	// this makes a vertical window
 	const int image_height = image_width / aspect_ratio;
+
+	// World
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+	world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
 	// camera
 	auto viewport_height = 2.0;
@@ -64,7 +52,7 @@ int main(){
 			auto v = double(j) / (image_height-1);
 			// origin and direction
 			ray r(origin, lower_left_cornet + u*horizontal + v*vertical - origin);
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
