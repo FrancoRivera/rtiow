f4c755abc44b2c411fc1107b32c70b26d619a8fc
Franco <franco@wemake.pe>
2022-09-25 16:12:50 -0500

    Add dialectric basic material, c10.2

--- material.h
+++ material.h
@@ -51,4 +51,30 @@ metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
     double fuzz;
 };
 
+class dialectric : public material {
+    public:
+dialectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    virtual bool scatter(
+   const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+) const override {
+        attenuation = color(1.0, 1.0, 1.0);
+
+        // get wether its comming from witihn the ball or not, adjust
+        // the value of the IR depending, if more desinty to less density
+        // by snells law the ir is flipped
+        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+    public:
+    double ir; // Index of refraction
+};
+
+
 #endif // MATERIAL_H
