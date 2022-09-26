b8d2a912a73104729a9ceca935d9adad0ba5ec03
francorivera <francoriverarivas@hotmail.com>
2022-09-25 20:06:42 -0500

    Fix some minor issues so Apple's clang wont cry

--- vec3.h
+++ vec3.h
@@ -10,8 +10,16 @@ using std::sqrt;
 
 class vec3{
  public:
-  vec3() : e{0,0,0} {}
-  vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+  vec3(){
+	  e[0] = 0;
+	  e[1] = 0;
+	  e[2] = 0;
+  };
+  vec3(double e0, double e1, double e2) {
+	  e[0] = e0;
+	  e[1] = e1;
+	  e[2] = e2;
+  }
 
   double x() const { return e[0]; }
   double y() const { return e[1]; }
@@ -83,7 +91,7 @@ inline vec3 operator*(const vec3 &v, double t){
 
 inline vec3 operator/(vec3 v, double t){
 	return (1/t) * v;
-}	
+}
 
 
 inline double dot(const vec3 &u, const vec3 &v) {
